9cd1a16775b76c16345efb6248b7ca42d8971f90
ScionRs <grazs116@gmail.com>
2021-06-03 14:52:48 +0300

    Added carousel-section

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 500,
+        autoPlay: true,
+        autoplaySpeed: 500,
+        fade:true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg" alt="left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg" alt="right"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
